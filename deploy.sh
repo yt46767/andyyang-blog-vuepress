eb88b67f3550c151bbcddfc3155389df0f471718
杨涛 <467675016@qq.com>
2022-09-18 17:27:53 +0800

    feat: 更新

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/yt46767/note.git master:gh-pages
+git push -f https://github.com/yt46767/andyyang-blog-vuepress.git master:gh-pages
 
 cd -
